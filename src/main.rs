e204bebcbfa45fe986eda0a0a651996caa3dd3e4
Simon Martin <smartin@scottlogic.com>
2020-04-03 07:17:58 +0000

    Add dir walking

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate getopts;
 use std::env;
+use walkdir::WalkDir;
 
 #[derive(Debug)]
 struct Options {
@@ -8,15 +9,8 @@ struct Options {
     limit: usize,
     pattern: String,
     hash_split_threshold: usize,
-    dry_run: bool
-}
-
-fn do_work(inp: &str, out: Option<String>) {
-    println!("{}", inp);
-    match out {
-        Some(x) => println!("{}", x),
-        None => println!("No Output"),
-    }
+    dry_run: bool,
+    paths: Vec<String>,
 }
 
 fn print_usage(program: &str, opts: getopts::Options) {
@@ -31,61 +25,106 @@ fn parse_command_line(args: Vec<String>) -> Option<Options> {
     let default_threshold = 1024;
     let default_limit = 0;
     let default_hash_split_threshold = 3;
-/*
-    let options = Options {
-        verbosity: 0,
-     threshold: 1024,
-     limit: 0,
-     hash_split_threshold: None,
-     dry_run: false
-    };
-*/
 
     let mut opts = getopts::Options::new();
-    opts.optopt("s", "pattern", &format!("Desired organisational structure. (Default: '{}')", default_pattern), "PATTERN");
-    opts.optopt("t", "threshold", &format!("Maximum files per group in organisation structure. (Default: {})", default_threshold), "THRESHOLD");
-    opts.optflag("", "dry-run", "Do not modify/move files, only show what would be done.");
-    opts.optflag("", "disable-group-dir", "Do not append an organisational numbered group");
+    opts.optopt(
+        "s",
+        "pattern",
+        &format!(
+            "Desired organisational structure. (Default: '{}')",
+            default_pattern
+        ),
+        "PATTERN",
+    );
+    opts.optopt(
+        "t",
+        "threshold",
+        &format!(
+            "Maximum files per group in organisation structure. (Default: {})",
+            default_threshold
+        ),
+        "THRESHOLD",
+    );
+    opts.optflag(
+        "",
+        "dry-run",
+        "Do not modify/move files, only show what would be done.",
+    );
+    opts.optflag(
+        "",
+        "disable-group-dir",
+        "Do not append an organisational numbered group",
+    );
     opts.optflag("", "case-sensitive", "Sort filenames case-sensitively");
-    opts.optopt("", "limit", &format!("Limit number of files to process (default: {})", default_limit), "LIMIT");
+    opts.optopt(
+        "",
+        "limit",
+        &format!(
+            "Limit number of files to process (default: {})",
+            default_limit
+        ),
+        "LIMIT",
+    );
     opts.optflagmulti("v", "", "increase verbosity");
     opts.optflag("h", "help", "print this help menu");
-    opts.optopt("", "hash-split", &format!("Position in SHA1 hash to split path (Default: {})", default_hash_split_threshold), "THRESHOLD");
+    opts.optopt(
+        "",
+        "hash-split",
+        &format!(
+            "Position in SHA1 hash to split path (Default: {})",
+            default_hash_split_threshold
+        ),
+        "THRESHOLD",
+    );
 
     let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(f) => { panic!(f.to_string()) }
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
     };
 
-    println!("verbosity: {}", matches.opt_count("v"));
     if matches.opt_present("h") {
         print_usage(&program, opts);
         return None;
     }
     let options = Options {
         verbosity: matches.opt_count("v"),
-        hash_split_threshold: matches.opt_get_default("hash-split", default_hash_split_threshold).unwrap_or(default_hash_split_threshold),
-        threshold: matches.opt_get_default("t", default_threshold).unwrap_or(default_threshold),
+        hash_split_threshold: matches
+            .opt_get_default("hash-split", default_hash_split_threshold)
+            .unwrap_or(default_hash_split_threshold),
+        threshold: matches
+            .opt_get_default("t", default_threshold)
+            .unwrap_or(default_threshold),
         dry_run: matches.opt_present("dry-run"),
-        limit: matches.opt_get_default("limit", default_limit).unwrap_or(default_limit),
-        pattern: matches.opt_get_default("s", default_pattern.clone()).unwrap_or(default_pattern)
+        limit: matches
+            .opt_get_default("limit", default_limit)
+            .unwrap_or(default_limit),
+        pattern: matches
+            .opt_get_default("s", default_pattern.clone())
+            .unwrap_or(default_pattern),
+        paths: matches.free,
     };
     Some(options)
-    /*
-    let output = matches.opt_str("o");
-    let input = if !matches.free.is_empty() {
-        matches.free[0].clone()
-    } else {
-        print_usage(&program, opts);
-        return;
-    };
-    do_work(&input, output);
-    */
+}
+
+fn process(root: &String, entry: &walkdir::DirEntry) -> Option<String> {
+    println!("{} -> {:?}", root, entry);
+    None
+}
+
+fn gather_files(options: Options) {
+    for path in options.paths {
+        let entries: Vec<_> = WalkDir::new(&path)
+            .into_iter()
+            .filter_map(|entry| entry.ok().map(|entry| process(&path, &entry)).flatten())
+            .collect();
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let options = parse_command_line(args);
-    println!("{:?}", options);
+    if let Some(options) = parse_command_line(args) {
+        println!("{:?}", options);
+        gather_files(options);
+    }
 }
